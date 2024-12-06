d9e29528af038e9efa187973328cb24d782dbe35
Ram <salunkherc@gmail.com>
2024-12-06 18:54:19 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
 import { Config } from './config';
+import app from './config/app';
 
-console.log(Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+       
+      console.log(`listening on port ${PORT}`);
+    });
+  } catch (err) {
+     
+    console.error(err);
+    process.exit(1);
+  }
+};
+startServer();

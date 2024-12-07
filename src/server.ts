0f9714f4eac7bd2c55945e8a971e907a0163674f
Ram <salunkherc@gmail.com>
2024-12-07 20:15:37 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import { Config } from './config';
 import app from './config/app';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
     app.listen(PORT, () => {
-       
-      console.log(`listening on port ${PORT}`);
+      logger.info('Server listening on port', { port: PORT });
     });
   } catch (err) {
-     
     console.error(err);
     process.exit(1);
   }

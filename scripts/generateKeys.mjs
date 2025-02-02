e91a04beea7a783842ec4d8ff96e75b4b75cce13
Ram <salunkherc@gmail.com>
2025-02-02 18:12:23 +0530

    generate key using crypto

---
+++ scripts/generateKeys.mjs
@@ -0,0 +1,21 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import { format } from 'path';
+
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+});
+
+console.log('private key >>', privateKey);
+console.log('public key >>', publicKey);
+
+fs.writeFileSync('certs/private.pem', privateKey);
+fs.writeFileSync('certs/public.pem', publicKey);
